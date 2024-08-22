ae40e73e89565f035fd4024e909aec219ca85db1
Miron Pawlik <miron.pawlik@swmansion.com>
2024-08-22 13:43:29 +0200

    SImplify video grid

--- examples/video-chat/components/VideosGrid.tsx
+++ examples/video-chat/components/VideosGrid.tsx
@@ -1,119 +1,57 @@
 import {
   Track,
   VideoRendererView,
-  setTargetTrackEncoding,
   Metadata,
 } from '@fishjam-cloud/react-native-client';
 import React from 'react';
-import { Dimensions, StyleSheet, View } from 'react-native';
-import Animated, {
-  FadeInDown,
-  LinearTransition,
-} from 'react-native-reanimated';
+import { FlatList, StyleSheet, View } from 'react-native';
 
-import LetterButton from './LetterButton';
 import { roomScreenLabels } from '../types/ComponentLabels';
+import { BrandColors } from '../utils/Colors';
 
 type Props = {
   tracks: Track<Metadata>[];
 };
 
-const { width } = Dimensions.get('window');
 const { VIDEO_CELL } = roomScreenLabels;
-const AnimatedVideoRenderer =
-  Animated.createAnimatedComponent(VideoRendererView);
 
 export default function VideosGrid({ tracks }: Props) {
-  const videoWidth = (width - 40) / 2;
-
+  console.log('track', tracks);
   return (
-    <View
-      style={
-        tracks.length > 3 ? styles.videosContainer2 : styles.videosContainer1
-      }>
-      {tracks.map((v, idx) => (
-        <Animated.View
+    <FlatList<Track<Metadata>>
+      data={tracks}
+      renderItem={({ item: track, index: idx }) => (
+        <View
           accessibilityLabel={VIDEO_CELL + idx}
-          entering={FadeInDown.duration(200)}
-          layout={LinearTransition.duration(150)}
-          style={
-            tracks.length > 3
-              ? [styles.video2, { width: videoWidth, height: videoWidth }]
-              : [styles.video1, { maxWidth: width - 20 }]
-          }
-          key={v.id}>
-          <AnimatedVideoRenderer
-            trackId={v.id}
-            entering={FadeInDown.duration(200)}
-            layout={LinearTransition.duration(150)}
-            style={styles.animatedView}
+          style={styles.video}
+          key={idx}>
+          <VideoRendererView
+            trackId={track.id}
+            videoLayout="FIT"
+            style={styles.flexOne}
           />
-          {(v.simulcastConfig?.enabled ?? false) && (
-            <View style={styles.buttons}>
-              {v.simulcastConfig?.activeEncodings.map((e) => (
-                <LetterButton
-                  key={e}
-                  trackEncoding={e}
-                  selected={v.encoding === e}
-                  onPress={() => setTargetTrackEncoding(v.id, e)}
-                />
-              ))}
-            </View>
-          )}
-        </Animated.View>
-      ))}
-    </View>
+        </View>
+      )}
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  animatedView: { flex: 1 },
+  flexOne: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
   },
-  videosContainer1: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  videosContainer2: {
-    flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    alignContent: 'center',
-  },
-  video1: {
-    flex: 1,
-    margin: 10,
-    aspectRatio: 1,
-    borderRadius: 8,
-    overflow: 'hidden',
-    backgroundColor: 'black',
-  },
-  video2: {
-    margin: 10,
-    aspectRatio: 1,
-    borderRadius: 8,
-    overflow: 'hidden',
-    backgroundColor: 'black',
-  },
-  border: {
-    borderRadius: 8,
-    overflow: 'hidden',
-  },
   video: {
     flex: 1,
-  },
-  buttons: {
-    flexDirection: 'row',
-    gap: 8,
-    padding: 4,
+    margin: 10,
+    aspectRatio: 1,
     borderRadius: 8,
-    position: 'absolute',
-    opacity: 0.5,
-    backgroundColor: 'white',
-    right: 0,
+    overflow: 'hidden',
+    backgroundColor: BrandColors.darkBlue60,
+    borderColor: BrandColors.darkBlue100,
+    borderWidth: 2,
   },
 });
