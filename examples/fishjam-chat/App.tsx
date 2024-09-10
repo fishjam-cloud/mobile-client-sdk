03f8cece372b1c86ed20e977aac8cfafce87e06f
Miron Pawlik <miron.pawlik@swmansion.com>
2024-09-10 10:17:47 +0200

    Add all dependencies

--- examples/fishjam-chat/App.tsx
+++ examples/fishjam-chat/App.tsx
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import { useReconnection } from "@fishjam-cloud/react-native-client";
 
 export default function App() {
+  const { reconnectionStatus } = useReconnection();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>
+        Open up App.tsx to start working on your app {reconnectionStatus}!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
