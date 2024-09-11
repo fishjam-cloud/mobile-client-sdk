8015fca963d012fdcc513f45a5d119a82c68f9db
Miron Pawlik <mironiasty@users.noreply.github.com>
2024-09-11 10:48:53 +0200

    [FCE-293] Upgrade expo and react native (#102)
    ## Description
    
    - Use latest Expo and React native. 
    - Also remove native `ios` and `android` files from test app.
    - Finish test app rename to FishjamChat
    
    Still require fixing:
    - icons on Android are broken
    - 
    ## Motivation and Context
    
    Let's have latest expo in use, so we can test it on new RN
    
    ## How has this been tested?
    
    App was build on iOS and Android. Basic e2e testing were done.
    
    
    ## Types of changes
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to
          not work as expected)
    
    ## Checklist:
    
    - [ ] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    
    ## Screenshots (if appropriate)

--- examples/fishjam-chat/index.js
+++ examples/fishjam-chat/index.js
@@ -1,17 +1,7 @@
-/**
- * @format
- */
+// This file is required due to monorepo environment.
+// If in future it would cause any issue, feel free to remove it.
+import registerRootComponent from 'expo/build/launch/registerRootComponent';
 
-import { AppRegistry } from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
-import { LogBox } from 'react-native';
-import notifee from '@notifee/react-native';
 
-LogBox.ignoreLogs(['new NativeEventEmitter']);
-
-notifee.registerForegroundService((notification) => {
-  return new Promise(() => {});
-});
-
-AppRegistry.registerComponent(appName, () => App);
+registerRootComponent(App);
