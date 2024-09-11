035f93b2f0ece5cad45835ed5b21047987cc169e
Miron Pawlik <miron.pawlik@swmansion.com>
2024-09-11 09:08:24 +0200

    Remove notifiee leftovers

--- examples/fishjam-chat/index.js
+++ examples/fishjam-chat/index.js
@@ -1,3 +1,5 @@
+// This file is required due to monorepo environment.
+// If in future it would cause any issue, feel free to remove it.
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
 
 import App from './App';
