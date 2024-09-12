163a7eb65a895d0eaa7411c8d8381146cdec33d9
Milosz Filimowski <milosz.filimowski@swmansion.com>
2024-09-12 13:02:27 +0200

    add ios plugin

--- packages/react-native-client/plugin/src/types.ts
+++ packages/react-native-client/plugin/src/types.ts
@@ -2,4 +2,8 @@ export type FishjamPluginOptions = {
   android: {
     enableForegroundService: boolean;
   };
+  ios: {
+    iPhoneDeploymentTarget?: string;
+    setUpScreensharing: boolean;
+  };
 };
