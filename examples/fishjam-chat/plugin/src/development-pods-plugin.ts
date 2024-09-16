dedca2bbf27a8ee50e13545038ac0cbbdcc6f517
Milosz Filimowski <milosz.filimowski@swmansion.com>
2024-09-16 11:48:04 +0200

    refactor dev plugin

--- examples/fishjam-chat/plugin/src/development-pods-plugin.ts
+++ examples/fishjam-chat/plugin/src/development-pods-plugin.ts
@@ -1,27 +1,36 @@
 import { ConfigPlugin, withPodfile } from '@expo/config-plugins';
 
+function replaceCloudClientForExtension(podfileContent: string) {
+  const targetName = 'FishjamScreenBroadcastExtension';
+  const podToReplace = "pod 'FishjamCloudClient/Broadcast'";
+  const replacementPod =
+    "pod 'FishjamCloudClient/Broadcast', :path => '../../../'";
+
+  const targetRegex = new RegExp(
+    `target '${targetName}' do[\\s\\S]*?${podToReplace}[\\s\\S]*?end`,
+    'g',
+  );
+
+  podfileContent = podfileContent.replace(targetRegex, (match) => {
+    return match.replace(podToReplace, replacementPod);
+  });
+  return podfileContent;
+}
+
+function replaceCloudClientForMainApp(podfileContent: string) {
+  podfileContent = podfileContent.replace(
+    /target ['"]FishjamChat['"] do/g,
+    (match) => `${match}\n  pod 'FishjamCloudClient', :path => '../../../'`,
+  );
+  return podfileContent;
+}
+
 const withCustomPodfile: ConfigPlugin = (config) => {
   return withPodfile(config, async (config) => {
     let podfile = config.modResults.contents;
 
-    const targetName = 'FishjamScreenBroadcastExtension';
-    const podToReplace = "pod 'FishjamCloudClient/Broadcast'";
-    const replacementPod =
-      "pod 'FishjamCloudClient/Broadcast', :path => '../../../'";
-
-    const targetRegex = new RegExp(
-      `target '${targetName}' do[\\s\\S]*?${podToReplace}[\\s\\S]*?end`,
-      'g',
-    );
-
-    podfile = podfile.replace(targetRegex, (match) => {
-      return match.replace(podToReplace, replacementPod);
-    });
-
-    podfile = podfile.replace(
-      /target ['"]FishjamChat['"] do/g,
-      (match) => `${match}\n  pod 'FishjamCloudClient', :path => '../../../'`,
-    );
+    podfile = replaceCloudClientForExtension(podfile);
+    podfile = replaceCloudClientForMainApp(podfile);
 
     config.modResults.contents = podfile;
     return config;
