647cee49c197b952b076dd388f5b4fb71cfc7f4e
Miron Pawlik <miron.pawlik@swmansion.com>
2024-09-16 10:05:44 +0200

    Make cameras readonly

--- packages/react-native-client/src/RNFishjamClientModule.ts
+++ packages/react-native-client/src/RNFishjamClientModule.ts
@@ -25,7 +25,7 @@ type RNFishjamClient = {
   toggleCamera: () => Promise<boolean>;
   flipCamera: () => Promise<void>;
   switchCamera: (cameraId: string) => Promise<void>;
-  cameras: Camera[];
+  cameras: ReadonlyArray<Camera>;
   handleScreencastPermission: () => Promise<'granted' | 'denied'>;
   toggleScreencast: (
     screencastOptions: Partial<ScreencastOptionsInternal>,
