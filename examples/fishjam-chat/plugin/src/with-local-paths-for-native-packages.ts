fb047454123bfa85a826495133e41767e9e415da
Milosz Filimowski <milosz.filimowski@swmansion.com>
2024-09-18 09:43:42 +0200

    rename plugin

--- examples/fishjam-chat/plugin/src/with-local-paths-for-native-packages.ts
+++ examples/fishjam-chat/plugin/src/with-local-paths-for-native-packages.ts
@@ -2,11 +2,11 @@ import { ConfigPlugin } from '@expo/config-plugins';
 import { withCustomConfigIOS } from './with-custom-config-ios';
 import { withCustomConfigAndroid } from './with-custom-config-android';
 
-const withCustomDevelopmentPaths: ConfigPlugin = (config) => {
+const withLocalPathsForNativePackages: ConfigPlugin = (config) => {
   config = withCustomConfigIOS(config);
   config = withCustomConfigAndroid(config);
 
   return config;
 };
 
-export default withCustomDevelopmentPaths;
+export default withLocalPathsForNativePackages;
