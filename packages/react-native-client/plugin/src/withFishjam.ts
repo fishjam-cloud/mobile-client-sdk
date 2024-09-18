47c8fb0828a06a47ac6d2657b72ef396bcf2e63c
Miłosz Filimowski <MiloszFilimowski@users.noreply.github.com>
2024-09-18 10:05:44 +0200

    [FCE-539]: Fix android local deps in example project (#116)
    ## Description
    
    - Added example app Android plugin
    - TODO: Created a follow-up issue
    [FCE-540](https://linear.app/swmansion/issue/FCE-540) to make both SDK
    and example plugins overridable, meaning we can run `expo prebuild`
    without `--clean` flag.
    
    ## Motivation and Context
    
    - Local Android SDK was not attached to example project. 
    
    ## How has this been tested?
    
    - Successfully built Android 
    
    ## Types of changes
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to
          not work as expected)
    
    ## Checklist:
    
    - [x] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    
    ## Screenshots (if appropriate)

--- packages/react-native-client/plugin/src/withFishjam.ts
+++ packages/react-native-client/plugin/src/withFishjam.ts
@@ -1,11 +1,11 @@
 import { ConfigPlugin } from '@expo/config-plugins';
 import { withFishjamAndroid } from './withFishjamAndroid';
 import { FishjamPluginOptions } from './types';
-import withFishjamIOS from './withFishjamIOS';
+import withFishjamIos from './withFishjamIos';
 
 const withFishjam: ConfigPlugin<FishjamPluginOptions> = (config, options) => {
   withFishjamAndroid(config, options);
-  withFishjamIOS(config, options);
+  withFishjamIos(config, options);
   return config;
 };
 
