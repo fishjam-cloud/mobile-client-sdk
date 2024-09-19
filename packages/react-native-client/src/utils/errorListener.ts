f223c6c5c8515789327eacfb5ad8d7af0a65722a
Milosz Filimowski <milosz.filimowski@swmansion.com>
2024-09-19 10:58:21 +0200

    add try catch

--- packages/react-native-client/src/utils/errorListener.ts
+++ packages/react-native-client/src/utils/errorListener.ts
@@ -2,12 +2,17 @@ import { ReceivableEvents } from '../hooks/useFishjamEvent';
 import { nativeModuleEventEmitter } from '../RNFishjamClientModule';
 
 export const initializeWarningListener = () => {
-  if (__DEV__) {
+  if (!__DEV__) {
+    return;
+  }
+  try {
     nativeModuleEventEmitter.addListener(
       ReceivableEvents.Warning,
       ({ message }: { message: string }) => {
         console.warn(message);
       },
     );
+  } catch (error) {
+    console.error(`Failed to start warning listener: ${error?.message ?? ''}`);
   }
 };
