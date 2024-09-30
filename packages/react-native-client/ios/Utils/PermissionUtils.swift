c06a8ce38adaab087323c8dd018567e38e6ed6d3
Milosz Filimowski <milosz.filimowski@swmansion.com>
2024-09-30 15:44:09 +0200

    cr improvements

--- packages/react-native-client/ios/Utils/PermissionUtils.swift
+++ packages/react-native-client/ios/Utils/PermissionUtils.swift
@@ -7,11 +7,10 @@ class PermissionUtils {
     private static func requestAccessIfNeeded(for mediaType: AVMediaType) async -> Bool {
         let status = AVCaptureDevice.authorizationStatus(for: mediaType)
         
-        var isAuthorized = status == .authorized
-        if status == .notDetermined {
-            isAuthorized = await AVCaptureDevice.requestAccess(for: mediaType)
+        return if status == .notDetermined {
+            await AVCaptureDevice.requestAccess(for: mediaType)
+        } else {
+            status == .authorized
         }
-        
-        return isAuthorized
     }
 }
