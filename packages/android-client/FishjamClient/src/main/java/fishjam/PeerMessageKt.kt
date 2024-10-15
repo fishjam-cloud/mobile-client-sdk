e14acbd7554a8a2ae88de12e709ec760ddbe2ab9
Miron Pawlik <miron.pawlik@swmansion.com>
2024-10-15 11:35:06 +0200

    Use latest protos

--- packages/android-client/FishjamClient/src/main/java/fishjam/PeerMessageKt.kt
+++ packages/android-client/FishjamClient/src/main/java/fishjam/PeerMessageKt.kt
@@ -3,16 +3,11 @@
 
 // Generated files should ignore deprecation warnings
 @file:Suppress("DEPRECATION")
-
-package fishjam
+package fishjam;
 
 @kotlin.jvm.JvmName("-initializepeerMessage")
 public inline fun peerMessage(block: fishjam.PeerMessageKt.Dsl.() -> kotlin.Unit): fishjam.PeerNotifications.PeerMessage =
-  fishjam.PeerMessageKt.Dsl
-    ._create(fishjam.PeerNotifications.PeerMessage.newBuilder())
-    .apply { block() }
-    ._build()
-
+  fishjam.PeerMessageKt.Dsl._create(fishjam.PeerNotifications.PeerMessage.newBuilder()).apply { block() }._build()
 /**
  * ```
  * Defines any type of message sent between FJ and a peer
@@ -42,24 +37,23 @@ public object PeerMessageKt {
     public var authenticated: fishjam.PeerNotifications.PeerMessage.Authenticated
       @JvmName("getAuthenticated")
       get() = _builder.getAuthenticated()
-
       @JvmName("setAuthenticated")
       set(value) {
         _builder.setAuthenticated(value)
       }
-
     /**
      * `.fishjam.PeerMessage.Authenticated authenticated = 1;`
      */
     public fun clearAuthenticated() {
       _builder.clearAuthenticated()
     }
-
     /**
      * `.fishjam.PeerMessage.Authenticated authenticated = 1;`
      * @return Whether the authenticated field is set.
      */
-    public fun hasAuthenticated(): kotlin.Boolean = _builder.hasAuthenticated()
+    public fun hasAuthenticated(): kotlin.Boolean {
+      return _builder.hasAuthenticated()
+    }
 
     /**
      * `.fishjam.PeerMessage.AuthRequest auth_request = 2;`
@@ -67,24 +61,23 @@ public object PeerMessageKt {
     public var authRequest: fishjam.PeerNotifications.PeerMessage.AuthRequest
       @JvmName("getAuthRequest")
       get() = _builder.getAuthRequest()
-
       @JvmName("setAuthRequest")
       set(value) {
         _builder.setAuthRequest(value)
       }
-
     /**
      * `.fishjam.PeerMessage.AuthRequest auth_request = 2;`
      */
     public fun clearAuthRequest() {
       _builder.clearAuthRequest()
     }
-
     /**
      * `.fishjam.PeerMessage.AuthRequest auth_request = 2;`
      * @return Whether the authRequest field is set.
      */
-    public fun hasAuthRequest(): kotlin.Boolean = _builder.hasAuthRequest()
+    public fun hasAuthRequest(): kotlin.Boolean {
+      return _builder.hasAuthRequest()
+    }
 
     /**
      * `.fishjam.PeerMessage.MediaEvent media_event = 3;`
@@ -92,25 +85,47 @@ public object PeerMessageKt {
     public var mediaEvent: fishjam.PeerNotifications.PeerMessage.MediaEvent
       @JvmName("getMediaEvent")
       get() = _builder.getMediaEvent()
-
       @JvmName("setMediaEvent")
       set(value) {
         _builder.setMediaEvent(value)
       }
-
     /**
      * `.fishjam.PeerMessage.MediaEvent media_event = 3;`
      */
     public fun clearMediaEvent() {
       _builder.clearMediaEvent()
     }
-
     /**
      * `.fishjam.PeerMessage.MediaEvent media_event = 3;`
      * @return Whether the mediaEvent field is set.
      */
-    public fun hasMediaEvent(): kotlin.Boolean = _builder.hasMediaEvent()
+    public fun hasMediaEvent(): kotlin.Boolean {
+      return _builder.hasMediaEvent()
+    }
 
+    /**
+     * `.fishjam.PeerMessage.RTCStatsReport rtc_stats_report = 4;`
+     */
+    public var rtcStatsReport: fishjam.PeerNotifications.PeerMessage.RTCStatsReport
+      @JvmName("getRtcStatsReport")
+      get() = _builder.getRtcStatsReport()
+      @JvmName("setRtcStatsReport")
+      set(value) {
+        _builder.setRtcStatsReport(value)
+      }
+    /**
+     * `.fishjam.PeerMessage.RTCStatsReport rtc_stats_report = 4;`
+     */
+    public fun clearRtcStatsReport() {
+      _builder.clearRtcStatsReport()
+    }
+    /**
+     * `.fishjam.PeerMessage.RTCStatsReport rtc_stats_report = 4;`
+     * @return Whether the rtcStatsReport field is set.
+     */
+    public fun hasRtcStatsReport(): kotlin.Boolean {
+      return _builder.hasRtcStatsReport()
+    }
     public val contentCase: fishjam.PeerNotifications.PeerMessage.ContentCase
       @JvmName("getContentCase")
       get() = _builder.getContentCase()
@@ -119,19 +134,9 @@ public object PeerMessageKt {
       _builder.clearContent()
     }
   }
-
   @kotlin.jvm.JvmName("-initializeauthenticated")
-  public inline fun authenticated(
-    block: fishjam.PeerMessageKt.AuthenticatedKt.Dsl.() -> kotlin.Unit
-  ): fishjam.PeerNotifications.PeerMessage.Authenticated =
-    fishjam.PeerMessageKt.AuthenticatedKt.Dsl
-      ._create(
-        fishjam.PeerNotifications.PeerMessage.Authenticated
-          .newBuilder()
-      ).apply {
-        block()
-      }._build()
-
+  public inline fun authenticated(block: fishjam.PeerMessageKt.AuthenticatedKt.Dsl.() -> kotlin.Unit): fishjam.PeerNotifications.PeerMessage.Authenticated =
+    fishjam.PeerMessageKt.AuthenticatedKt.Dsl._create(fishjam.PeerNotifications.PeerMessage.Authenticated.newBuilder()).apply { block() }._build()
   /**
    * ```
    * Response sent by FJ, confirming successfull authentication
@@ -156,18 +161,9 @@ public object PeerMessageKt {
       internal fun _build(): fishjam.PeerNotifications.PeerMessage.Authenticated = _builder.build()
     }
   }
-
   @kotlin.jvm.JvmName("-initializeauthRequest")
-  public inline fun authRequest(
-    block: fishjam.PeerMessageKt.AuthRequestKt.Dsl.() -> kotlin.Unit
-  ): fishjam.PeerNotifications.PeerMessage.AuthRequest =
-    fishjam.PeerMessageKt.AuthRequestKt.Dsl
-      ._create(
-        fishjam.PeerNotifications.PeerMessage.AuthRequest
-          .newBuilder()
-      ).apply { block() }
-      ._build()
-
+  public inline fun authRequest(block: fishjam.PeerMessageKt.AuthRequestKt.Dsl.() -> kotlin.Unit): fishjam.PeerNotifications.PeerMessage.AuthRequest =
+    fishjam.PeerMessageKt.AuthRequestKt.Dsl._create(fishjam.PeerNotifications.PeerMessage.AuthRequest.newBuilder()).apply { block() }._build()
   /**
    * ```
    * Request sent by peer, to authenticate to FJ server
@@ -197,12 +193,10 @@ public object PeerMessageKt {
       public var token: kotlin.String
         @JvmName("getToken")
         get() = _builder.getToken()
-
         @JvmName("setToken")
         set(value) {
           _builder.setToken(value)
         }
-
       /**
        * `string token = 1;`
        */
@@ -211,18 +205,9 @@ public object PeerMessageKt {
       }
     }
   }
-
   @kotlin.jvm.JvmName("-initializemediaEvent")
-  public inline fun mediaEvent(
-    block: fishjam.PeerMessageKt.MediaEventKt.Dsl.() -> kotlin.Unit
-  ): fishjam.PeerNotifications.PeerMessage.MediaEvent =
-    fishjam.PeerMessageKt.MediaEventKt.Dsl
-      ._create(
-        fishjam.PeerNotifications.PeerMessage.MediaEvent
-          .newBuilder()
-      ).apply { block() }
-      ._build()
-
+  public inline fun mediaEvent(block: fishjam.PeerMessageKt.MediaEventKt.Dsl.() -> kotlin.Unit): fishjam.PeerNotifications.PeerMessage.MediaEvent =
+    fishjam.PeerMessageKt.MediaEventKt.Dsl._create(fishjam.PeerNotifications.PeerMessage.MediaEvent.newBuilder()).apply { block() }._build()
   /**
    * ```
    * Any type of WebRTC messages passed betweend FJ and peer
@@ -252,12 +237,55 @@ public object PeerMessageKt {
       public var data: kotlin.String
         @JvmName("getData")
         get() = _builder.getData()
-
         @JvmName("setData")
         set(value) {
           _builder.setData(value)
         }
+      /**
+       * `string data = 1;`
+       */
+      public fun clearData() {
+        _builder.clearData()
+      }
+    }
+  }
+  @kotlin.jvm.JvmName("-initializerTCStatsReport")
+  public inline fun rTCStatsReport(block: fishjam.PeerMessageKt.RTCStatsReportKt.Dsl.() -> kotlin.Unit): fishjam.PeerNotifications.PeerMessage.RTCStatsReport =
+    fishjam.PeerMessageKt.RTCStatsReportKt.Dsl._create(fishjam.PeerNotifications.PeerMessage.RTCStatsReport.newBuilder()).apply { block() }._build()
+  /**
+   * ```
+   * PeerConnection stats sent by peer
+   * https://developer.mozilla.org/en-US/docs/Web/API/RTCStatsReport#the_statistic_types
+   * ```
+   *
+   * Protobuf type `fishjam.PeerMessage.RTCStatsReport`
+   */
+  public object RTCStatsReportKt {
+    @kotlin.OptIn(com.google.protobuf.kotlin.OnlyForUseByGeneratedProtoCode::class)
+    @com.google.protobuf.kotlin.ProtoDslMarker
+    public class Dsl private constructor(
+      private val _builder: fishjam.PeerNotifications.PeerMessage.RTCStatsReport.Builder
+    ) {
+      public companion object {
+        @kotlin.jvm.JvmSynthetic
+        @kotlin.PublishedApi
+        internal fun _create(builder: fishjam.PeerNotifications.PeerMessage.RTCStatsReport.Builder): Dsl = Dsl(builder)
+      }
 
+      @kotlin.jvm.JvmSynthetic
+      @kotlin.PublishedApi
+      internal fun _build(): fishjam.PeerNotifications.PeerMessage.RTCStatsReport = _builder.build()
+
+      /**
+       * `string data = 1;`
+       */
+      public var data: kotlin.String
+        @JvmName("getData")
+        get() = _builder.getData()
+        @JvmName("setData")
+        set(value) {
+          _builder.setData(value)
+        }
       /**
        * `string data = 1;`
        */
@@ -267,42 +295,25 @@ public object PeerMessageKt {
     }
   }
 }
+@kotlin.jvm.JvmSynthetic
+public inline fun fishjam.PeerNotifications.PeerMessage.copy(block: `fishjam`.PeerMessageKt.Dsl.() -> kotlin.Unit): fishjam.PeerNotifications.PeerMessage =
+  `fishjam`.PeerMessageKt.Dsl._create(this.toBuilder()).apply { block() }._build()
 
 @kotlin.jvm.JvmSynthetic
-public inline fun fishjam.PeerNotifications.PeerMessage.copy(
-  block: `fishjam`.PeerMessageKt.Dsl.() -> kotlin.Unit
-): fishjam.PeerNotifications.PeerMessage =
-  `fishjam`.PeerMessageKt.Dsl
-    ._create(this.toBuilder())
-    .apply { block() }
-    ._build()
+public inline fun fishjam.PeerNotifications.PeerMessage.Authenticated.copy(block: `fishjam`.PeerMessageKt.AuthenticatedKt.Dsl.() -> kotlin.Unit): fishjam.PeerNotifications.PeerMessage.Authenticated =
+  `fishjam`.PeerMessageKt.AuthenticatedKt.Dsl._create(this.toBuilder()).apply { block() }._build()
 
 @kotlin.jvm.JvmSynthetic
-public inline fun fishjam.PeerNotifications.PeerMessage.Authenticated.copy(
-  block: `fishjam`.PeerMessageKt.AuthenticatedKt.Dsl.() -> kotlin.Unit
-): fishjam.PeerNotifications.PeerMessage.Authenticated =
-  `fishjam`.PeerMessageKt.AuthenticatedKt.Dsl
-    ._create(this.toBuilder())
-    .apply { block() }
-    ._build()
+public inline fun fishjam.PeerNotifications.PeerMessage.AuthRequest.copy(block: `fishjam`.PeerMessageKt.AuthRequestKt.Dsl.() -> kotlin.Unit): fishjam.PeerNotifications.PeerMessage.AuthRequest =
+  `fishjam`.PeerMessageKt.AuthRequestKt.Dsl._create(this.toBuilder()).apply { block() }._build()
 
 @kotlin.jvm.JvmSynthetic
-public inline fun fishjam.PeerNotifications.PeerMessage.AuthRequest.copy(
-  block: `fishjam`.PeerMessageKt.AuthRequestKt.Dsl.() -> kotlin.Unit
-): fishjam.PeerNotifications.PeerMessage.AuthRequest =
-  `fishjam`.PeerMessageKt.AuthRequestKt.Dsl
-    ._create(this.toBuilder())
-    .apply { block() }
-    ._build()
+public inline fun fishjam.PeerNotifications.PeerMessage.MediaEvent.copy(block: `fishjam`.PeerMessageKt.MediaEventKt.Dsl.() -> kotlin.Unit): fishjam.PeerNotifications.PeerMessage.MediaEvent =
+  `fishjam`.PeerMessageKt.MediaEventKt.Dsl._create(this.toBuilder()).apply { block() }._build()
 
 @kotlin.jvm.JvmSynthetic
-public inline fun fishjam.PeerNotifications.PeerMessage.MediaEvent.copy(
-  block: `fishjam`.PeerMessageKt.MediaEventKt.Dsl.() -> kotlin.Unit
-): fishjam.PeerNotifications.PeerMessage.MediaEvent =
-  `fishjam`.PeerMessageKt.MediaEventKt.Dsl
-    ._create(this.toBuilder())
-    .apply { block() }
-    ._build()
+public inline fun fishjam.PeerNotifications.PeerMessage.RTCStatsReport.copy(block: `fishjam`.PeerMessageKt.RTCStatsReportKt.Dsl.() -> kotlin.Unit): fishjam.PeerNotifications.PeerMessage.RTCStatsReport =
+  `fishjam`.PeerMessageKt.RTCStatsReportKt.Dsl._create(this.toBuilder()).apply { block() }._build()
 
 public val fishjam.PeerNotifications.PeerMessageOrBuilder.authenticatedOrNull: fishjam.PeerNotifications.PeerMessage.Authenticated?
   get() = if (hasAuthenticated()) getAuthenticated() else null
@@ -312,3 +323,7 @@ public val fishjam.PeerNotifications.PeerMessageOrBuilder.authRequestOrNull: fis
 
 public val fishjam.PeerNotifications.PeerMessageOrBuilder.mediaEventOrNull: fishjam.PeerNotifications.PeerMessage.MediaEvent?
   get() = if (hasMediaEvent()) getMediaEvent() else null
+
+public val fishjam.PeerNotifications.PeerMessageOrBuilder.rtcStatsReportOrNull: fishjam.PeerNotifications.PeerMessage.RTCStatsReport?
+  get() = if (hasRtcStatsReport()) getRtcStatsReport() else null
+
