54a074a3186619a5d49bd348574ee5c1581f1405
Miron Pawlik <miron.pawlik@swmansion.com>
2024-10-29 18:08:07 +0100

    Use correct import from main example file

--- examples/fishjam-chat/index.js
+++ examples/fishjam-chat/index.js
@@ -1,6 +1,6 @@
 // This file is required due to monorepo environment.
 // If in future it would cause any issue, feel free to remove it.
-import registerRootComponent from 'expo/build/launch/registerRootComponent';
+import { registerRootComponent } from 'expo';
 
 import App from './App';
 
