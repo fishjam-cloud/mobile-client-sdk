b51453f78d40c294ea2258d9a08b547b09529344
Milosz Filimowski <milosz.filimowski@swmansion.com>
2024-10-30 17:14:32 +0100

    WIP 2

--- examples/fishjam-chat/components/VideosGrid/parsePeersToTracks.ts
+++ examples/fishjam-chat/components/VideosGrid/parsePeersToTracks.ts
@@ -1,11 +1,35 @@
-import { PeerWithTracks } from '@fishjam-cloud/react-native-client/build/hooks/usePeers';
+import {
+  AudioTrack,
+  PeerWithTracks,
+  VideoTrack,
+} from '@fishjam-cloud/react-native-client/build/hooks/usePeers';
 import { GridTrack } from './GridTrackItem';
 import { PeerMetadata } from '../../types/metadata';
+
+const createGridTrackFromPeer = <
+  T extends PeerWithTracks<PeerMetadata>,
+  VideoTrackKey extends keyof T,
+  AudioTrackKey extends keyof T,
+>(
+  peer: T,
+  videoTrack?: T[VideoTrackKey] & VideoTrack,
+  audioTrack?: T[AudioTrackKey] & AudioTrack,
+): GridTrack => {
+  return {
+    ...videoTrack,
+    peerId: peer.id,
+    isLocal: peer.isLocal,
+    userName: peer.metadata.peer.displayName,
+    isVadActive: audioTrack?.vadStatus === 'speech',
+  };
+};
 
 const createGridTrackFromPeer = (
   peer: PeerWithTracks<PeerMetadata>,
 ): GridTrack[] => {
   const tracks: GridTrack[] = [];
+
+  createGridTrackFromPeer(peer, peer.cameraTrack, peer.microphoneTrack);
 
   if (peer.cameraTrack && peer.cameraTrack.isActive) {
     tracks.push({
