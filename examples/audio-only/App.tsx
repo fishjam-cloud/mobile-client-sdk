0ec44ac8b01a7b361a9de5ff0028ed58830d87d9
Milosz Filimowski <milosz.filimowski@swmansion.com>
2025-03-27 14:00:04 +0100

    add audio device selector

--- examples/audio-only/App.tsx
+++ examples/audio-only/App.tsx
@@ -1,9 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
+import { AudioDevicePicker } from "./components/AudioDevicePicker";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Audio Device Selection</Text>
+      <AudioDevicePicker />
     </View>
   );
 }
