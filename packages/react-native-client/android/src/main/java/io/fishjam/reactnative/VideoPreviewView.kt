81bc655d75fc9a50d8735511bbecbf271e52a13f
Milosz Filimowski <milosz.filimowski@swmansion.com>
2025-03-31 17:06:16 +0200

    lint fix

--- packages/react-native-client/android/src/main/java/io/fishjam/reactnative/VideoPreviewView.kt
+++ packages/react-native-client/android/src/main/java/io/fishjam/reactnative/VideoPreviewView.kt
@@ -2,7 +2,6 @@ package io.fishjam.reactnative
 
 import android.content.Context
 import com.fishjamcloud.client.media.LocalVideoTrack
-import com.fishjamcloud.client.media.VideoTrack
 import expo.modules.kotlin.AppContext
 import io.fishjam.reactnative.managers.LocalCameraTrackChangedListener
 
@@ -11,14 +10,15 @@ class VideoPreviewView(
   appContext: AppContext
 ) : VideoRendererView(context, appContext),
   LocalCameraTrackChangedListener {
-    private fun trySetLocalCameraTrack() {
-
-      (RNFishjamClient.fishjamClient.getLocalEndpoint().tracks.values.firstOrNull { track ->
-          track is LocalVideoTrack
-        } as? LocalVideoTrack?)?.let {
-        init(it.id())
-      }
+  private fun trySetLocalCameraTrack() {
+    (
+      RNFishjamClient.fishjamClient.getLocalEndpoint().tracks.values.firstOrNull { track ->
+        track is LocalVideoTrack
+      } as? LocalVideoTrack?
+    )?.let {
+      init(it.id())
     }
+  }
 
   override fun onDetachedFromWindow() {
     RNFishjamClient.localCameraTracksChangedListenersManager.remove(this)
