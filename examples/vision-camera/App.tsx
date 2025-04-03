ff9d524b79ec91072dcf3e42fd8d37e352b1acf0
Milosz Filimowski <milosz.filimowski@swmansion.com>
2025-04-03 16:21:17 +0200

    working android

--- examples/vision-camera/App.tsx
+++ examples/vision-camera/App.tsx
@@ -3,15 +3,28 @@ import { StyleSheet } from "react-native";
 import {
   Camera,
   CameraPermissionStatus,
+  Frame,
   useCameraDevice,
   useFrameProcessor,
+  VisionCameraProxy,
 } from "react-native-vision-camera";
+
+const plugin = VisionCameraProxy.initFrameProcessorPlugin("sendFrame", {});
+
+export function sendFrame(frame: Frame) {
+  "worklet";
+  if (plugin === null)
+    throw new Error('Failed to load Frame Processor Plugin "scanFaces"!');
+  return plugin!.call(frame);
+}
 
 export default function App() {
   const device = useCameraDevice("back");
 
   const [cameraPermissionStatus, setCameraPermissionStatus] =
     useState<CameraPermissionStatus>("not-determined");
+
+  console.log({ cameraPermissionStatus });
 
   useEffect(() => {
     const requestCameraPermission = async () => {
@@ -24,7 +37,7 @@ export default function App() {
 
   const frameProcessor = useFrameProcessor((frame) => {
     "worklet";
-    console.log(`Frame: ${frame.width}x${frame.height} (${frame.pixelFormat})`);
+    sendFrame(frame);
   }, []);
 
   if (device && cameraPermissionStatus === "granted") {
--- examples/vision-camera/babel.config.js
+++ examples/vision-camera/babel.config.js
@@ -1,5 +1,7 @@
-module.exports = {
-    plugins: [
-        ['react-native-worklets-core/plugin'],
-    ],
-}
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: ['react-native-worklets-core/plugin']
+    };
+};
