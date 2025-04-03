8a4ae4c1a4be79537cd5a84baa4e11a755784784
Milosz Filimowski <milosz.filimowski@swmansion.com>
2025-04-03 18:07:56 +0200

    kinda working

--- examples/vision-camera/App.tsx
+++ examples/vision-camera/App.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
-import { StyleSheet } from "react-native";
+import { useConnection } from "@fishjam-cloud/react-native-client";
+import { useCallback, useEffect, useState } from "react";
+import { Button, View } from "react-native";
 import {
   Camera,
   CameraPermissionStatus,
@@ -11,6 +12,41 @@ import {
 
 const plugin = VisionCameraProxy.initFrameProcessorPlugin("sendFrame", {});
 
+// Check https://cloud.fishjam.work/app/ for your app ID
+const YOUR_APP_ID = "ea94930e3ffd47c490f5144c43d57340";
+
+export function JoinRoomButton() {
+  const { joinRoom, leaveRoom } = useConnection();
+
+  useEffect(() => () => leaveRoom(), [leaveRoom]);
+
+  const onPressJoin = useCallback(async () => {
+    const data = await getRoomDetails("Room", "User");
+    const { url, peerToken } = data;
+
+    console.log({ data });
+    console.log("received url: " + url);
+
+    await joinRoom({ url, peerToken });
+
+    console.log("joined room");
+  }, [joinRoom]);
+
+  return (
+    <View style={{ flex: 1 }}>
+      <Button onPress={onPressJoin} title="Join Room" />
+    </View>
+  );
+}
+
+async function getRoomDetails(roomName: string, peerName: string) {
+  const response = await fetch(
+    `https://room.fishjam.io/api/rooms?roomName=${roomName}&peerName=${peerName}`,
+  );
+  const { url, peerToken } = await response.json();
+  return { url, peerToken };
+}
+
 export function sendFrame(frame: Frame) {
   "worklet";
   if (plugin === null)
@@ -21,10 +57,10 @@ export function sendFrame(frame: Frame) {
 export default function App() {
   const device = useCameraDevice("back");
 
+  const { peerStatus } = useConnection();
+
   const [cameraPermissionStatus, setCameraPermissionStatus] =
     useState<CameraPermissionStatus>("not-determined");
-
-  console.log({ cameraPermissionStatus });
 
   useEffect(() => {
     const requestCameraPermission = async () => {
@@ -35,19 +71,27 @@ export default function App() {
     requestCameraPermission();
   });
 
-  const frameProcessor = useFrameProcessor((frame) => {
-    "worklet";
-    sendFrame(frame);
-  }, []);
+  const frameProcessor = useFrameProcessor(
+    (frame) => {
+      "worklet";
+      if (peerStatus === "connected") {
+        sendFrame(frame);
+      }
+    },
+    [peerStatus],
+  );
 
   if (device && cameraPermissionStatus === "granted") {
     return (
-      <Camera
-        style={StyleSheet.absoluteFill}
-        device={device}
-        isActive={true}
-        frameProcessor={frameProcessor}
-      />
+      <View style={{ flex: 1 }}>
+        <Camera
+          style={{ flex: 4 }}
+          device={device}
+          isActive={true}
+          frameProcessor={frameProcessor}
+        />
+        <JoinRoomButton />
+      </View>
     );
   }
   return null;
--- examples/vision-camera/modules/webrtc-source/android/src/main/java/io/fishjam/example/webrtcsource/webrtcframeprocessor/WebrtcFrameProcessorPlugin.kt
+++ examples/vision-camera/modules/webrtc-source/android/src/main/java/io/fishjam/example/webrtcsource/webrtcframeprocessor/WebrtcFrameProcessorPlugin.kt
@@ -1,13 +1,39 @@
 package io.fishjam.example.webrtcsource.webrtcframeprocessor
 
-import android.util.Log
+import androidx.camera.core.ImageProxy
+import com.fishjamcloud.client.models.CustomSource
+import com.fishjamcloud.client.models.CustomSourceConsumer
+import com.fishjamcloud.client.models.Metadata
+import com.fishjamcloud.client.models.VideoParameters
 import com.mrousavy.camera.frameprocessors.Frame
 import com.mrousavy.camera.frameprocessors.FrameProcessorPlugin
 import com.mrousavy.camera.frameprocessors.VisionCameraProxy
+import io.fishjam.reactnative.RNFishjamClient
+import kotlinx.coroutines.runBlocking
 
-class WebrtcFrameProcessorPlugin(proxy: VisionCameraProxy, options: Map<String, Any>?): FrameProcessorPlugin() {
+class WebrtcFrameProcessorPlugin(proxy: VisionCameraProxy, options: Map<String, Any>?): FrameProcessorPlugin(), CustomSource {
+  override val isScreenShare = false
+  override val metadata: Metadata = emptyMap()
+  override val videoParameters = VideoParameters.presetHD43
+
+  private var consumer: CustomSourceConsumer? = null
+
+  override fun initialize(consumer: CustomSourceConsumer) {
+    this.consumer = consumer
+  }
+
+  init {
+    runBlocking {
+      RNFishjamClient.createCustomSource(this@WebrtcFrameProcessorPlugin)
+    }
+  }
+
   override fun callback(frame: Frame, arguments: Map<String, Any>?): Any? {
-    Log.e("Webrtc", "works")
+
+    val imageProxy: ImageProxy = frame.imageProxy
+
+    consumer?.onImageProxyCaptured(imageProxy)
+
     return null
   }
 }
