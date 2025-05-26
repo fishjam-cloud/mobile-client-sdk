624df44845a0f63dbff2f7e5765f3f7641e83c2e
Miłosz Filimowski <MiloszFilimowski@users.noreply.github.com>
2025-05-26 12:16:41 +0200

    FCE-1471: Integrate react-native-whip-whep into react-native SDK (#387)
    ## Description
    
    - Integrated WHEP client into our SDK
    
    ## Motivation and Context
    
    - We want to support a simple livestreaming interface for our users
    
    ## How has this been tested?
    
    Run and tested on Android and iOS
    
    ## Types of changes
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to
          not work as expected)
    
    ## Checklist:
    
    - [x] My code follows the code style of this project.
    - [x] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.

---
+++ examples/fishjam-chat/screens/LivestreamScreen/LivestreamScreen.tsx
@@ -0,0 +1,91 @@
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import React, { useCallback, useEffect } from 'react';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { AppRootStackParamList } from '../../navigators/AppNavigator';
+import { BrandColors } from '../../utils/Colors';
+import {
+  useLivestream,
+  LivestreamView,
+} from '@fishjam-cloud/react-native-client';
+
+type Props = NativeStackScreenProps<AppRootStackParamList, 'LivestreamScreen'>;
+
+export default function LivestreamScreen({ route }: Props) {
+  const { livestreamUrl, viewerToken } = route.params;
+
+  const { connect, disconnect } = useLivestream(livestreamUrl, viewerToken);
+
+  const handleConnect = useCallback(async () => {
+    try {
+      await connect();
+    } catch (err) {
+      console.log(err);
+    }
+  }, [connect]);
+
+  useEffect(() => {
+    handleConnect();
+
+    return () => {
+      disconnect();
+    };
+  }, [handleConnect, disconnect]);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.box}>
+        <View style={styles.videoView}>
+          <LivestreamView style={styles.whepView} />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F1FAFE',
+    padding: 24,
+  },
+  box: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 20,
+  },
+  videoView: {
+    width: '100%',
+    height: '70%',
+    backgroundColor: '#E0E0E0',
+    borderRadius: 12,
+    overflow: 'hidden',
+    borderWidth: 1,
+    borderColor: BrandColors.darkBlue80,
+  },
+  whepView: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+  pausedVideo: {
+    flex: 1,
+    backgroundColor: '#333',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  connectedText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  pausedText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  controls: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    gap: 20,
+  },
+});
