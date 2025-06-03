f3cbaf0d64c7935ecdf7a0c4a9abf122bd3e2663
Miłosz Filimowski <MiloszFilimowski@users.noreply.github.com>
2025-06-03 12:45:56 +0200

    FCE-1584: Add missing LivestramView docs (#397)
    ## Description
    
    - Added missing docs
    
    ## Motivation and Context
    
    - Docs were missing
    
    ---------
    
    Co-authored-by: adrian <33912477+czerwiukk@users.noreply.github.com>

--- packages/react-native-client/src/components/LivestreamView.tsx
+++ packages/react-native-client/src/components/LivestreamView.tsx
@@ -2,10 +2,23 @@ import { CSSProperties } from 'react';
 import { StyleProp, ViewStyle } from 'react-native';
 import { WhepClientView } from 'react-native-whip-whep';
 
+/**
+ * Props of the LivestreamView component
+ */
 export type LivestreamViewProps = {
+  /**
+   * Styles of the LivestreamView component
+   */
   style?: StyleProp<ViewStyle>;
 };
 
+/**
+ * Renders a video player playing the livestream set up with {@link useLivestream} hook.
+ *
+ * @category Components
+ * @param {object} props
+ * @param {object} props.style
+ */
 export const LivestreamView = ({ style }: LivestreamViewProps) => (
   <WhepClientView style={style as CSSProperties} />
 );
