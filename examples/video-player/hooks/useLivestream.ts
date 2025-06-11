431e22cd58ffc6e659f99552a02a00e098708614
Dawid Matyjasik <dawidspisak@gmail.com>
2025-06-11 16:23:50 +0200

    feat: use livestream

---
+++ examples/video-player/hooks/useLivestream.ts
@@ -0,0 +1,83 @@
+import { useState, useEffect, useCallback, useRef } from "react";
+import {
+  createWhepClient,
+  disconnectWhepClient,
+  connectWhepClient,
+  useEvent,
+} from "react-native-whip-whep";
+
+const BROADCASTER_URL = "https://server.fishjam.stream/broadcaster";
+const ROOM_MANAGER_URL = "https://server.fishjam.stream/api/rooms";
+const ROOM_ID = "appjs";
+
+export const VIEWER_TOKEN_URL = `${ROOM_MANAGER_URL}/${ROOM_ID}/broadcast-viewer-token`;
+
+export const useLivestream = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isReconnecting, setIsReconnecting] = useState(false);
+  const [hasErrors, setHasErrors] = useState(false);
+  const isClientInitialized = useRef(false);
+
+  const resolveBroadcasterURLAfterRedirect = async () => {
+    const response = await fetch(BROADCASTER_URL);
+    if (!response.ok) {
+      throw new Error("Failed to fetch broadcaster URL");
+    }
+    return response.url;
+  };
+
+  const connectLivestream = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      setHasErrors(false);
+
+      const response = await fetch(VIEWER_TOKEN_URL);
+      if (!response.ok) {
+        throw new Error("Failed to fetch auth token");
+      }
+      const responseJson = await response.json();
+      const authToken = responseJson.token;
+
+      if (!authToken) {
+        throw new Error("Auth token response invalid");
+      }
+
+      const broadcasterURL = `${await resolveBroadcasterURLAfterRedirect()}/api/whep`;
+
+      createWhepClient(broadcasterURL, {
+        authToken,
+      });
+
+      await connectWhepClient();
+      isClientInitialized.current = true;
+    } catch (error) {
+      console.log(error);
+      setHasErrors(true);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEvent("reconnectionStatusChanged", (event) => {
+    if (event.status === "reconnectionStarted") {
+      setIsReconnecting(true);
+    } else if (event.status === "reconnected") {
+      setIsReconnecting(false);
+    } else if (event.status === "reconnectionRetriesLimitReached") {
+      setIsReconnecting(false);
+      setHasErrors(true);
+    }
+  });
+
+  useEffect(() => {
+    connectLivestream();
+
+    return () => {
+      if (isClientInitialized.current) {
+        disconnectWhepClient();
+      }
+    };
+  }, [connectLivestream]);
+
+  return { isLoading, isReconnecting, hasErrors, restart: connectLivestream };
+};
