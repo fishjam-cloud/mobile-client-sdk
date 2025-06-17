5ebdac1b1d27be47dcb05966ab99f04973b4465a
Milosz Filimowski <milosz.filimowski@swmansion.com>
2025-06-17 12:36:30 +0200

    fix docs

--- packages/react-native-client/src/components/LivestreamView.tsx
+++ packages/react-native-client/src/components/LivestreamView.tsx
@@ -1,18 +1,43 @@
 import { CSSProperties } from 'react';
 import { StyleProp, ViewStyle } from 'react-native';
-import {
-  ReactNativeMobileWhepClientViewProps,
-  WhepClientView,
-} from 'react-native-whip-whep';
+import { WhepClientView } from 'react-native-whip-whep';
 
 /**
  * Props of the LivestreamView component
  */
-export type LivestreamViewProps = ReactNativeMobileWhepClientViewProps & {
+export type LivestreamViewProps = {
   /**
    * Styles of the LivestreamView component
    */
   style?: StyleProp<ViewStyle>;
+  /**
+   * Used to override the orientation of the video (from metadata).
+   * Defaults to "portrait".
+   */
+  orientation?: 'landscape' | 'portrait';
+  /**
+   * A variable deciding whether the Picture-in-Picture is enabled.
+   * Defaults to true.
+   */
+  pipEnabled?: boolean;
+  /**
+   * A variable deciding whether the Picture-in-Picture mode should be started automatically after the app is backgrounded.
+   * Defaults to false.
+   */
+  autoStartPip?: boolean;
+  /**
+   * A variable deciding whether the Picture-in-Picture mode should be stopped automatically on iOS after the app is foregrounded.
+   * Always enabled on Android as PiP is not supported in foreground.
+   * Defaults to false.
+   */
+  autoStopPip?: boolean;
+  /**
+   * A variable deciding the size of the Picture-in-Picture mode.
+   */
+  pipSize?: {
+    width: number;
+    height: number;
+  };
 };
 
 /**
@@ -22,6 +47,20 @@ export type LivestreamViewProps = ReactNativeMobileWhepClientViewProps & {
  * @param {object} props
  * @param {object} props.style
  */
-export const LivestreamView = ({ style }: LivestreamViewProps) => (
-  <WhepClientView style={style as CSSProperties} />
+export const LivestreamView = ({
+  style,
+  orientation,
+  pipEnabled,
+  autoStartPip,
+  autoStopPip,
+  pipSize,
+}: LivestreamViewProps) => (
+  <WhepClientView
+    style={style as CSSProperties}
+    orientation={orientation}
+    pipEnabled={pipEnabled}
+    autoStartPip={autoStartPip}
+    autoStopPip={autoStopPip}
+    pipSize={pipSize}
+  />
 );
