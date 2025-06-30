7307aa666b180190bcfc3793684ca9d64748b5ca
Dawid Matyjasik <dawidspisak@gmail.com>
2025-06-30 11:25:31 +0200

    feat: video room example

--- examples/video-room/App.tsx
+++ examples/video-room/App.tsx
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import RootNavigation from './navigation/RootNavigation';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <RootNavigation />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
