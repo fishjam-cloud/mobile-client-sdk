8e7648ffdabb9e483e9f9d1f24132e361c227e13
Dawid Matyjasik <dawidspisak@gmail.com>
2025-06-30 11:23:58 +0200

    feat: videos grid

---
+++ examples/video-room/components/VideosGridItem.tsx
@@ -0,0 +1,74 @@
+import { VideoRendererView } from '@fishjam-cloud/react-native-client';
+import { useMemo } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { GridTrack } from '../types';
+
+export const VideosGridItem = ({ peer }: { peer: GridTrack }) => {
+  const videoContainer = useMemo(
+    () => [
+      styles.video,
+      {
+        backgroundColor: peer.isLocal ? '#606619' : '#7089DB',
+      },
+    ],
+    [peer.isLocal],
+  );
+
+  return (
+    <View style={styles.container}>
+      <View style={videoContainer}>
+        {peer.track ? (
+          <VideoRendererView
+            trackId={peer.track.id}
+            videoLayout="FIT"
+            style={styles.videoContent}
+          />
+        ) : (
+          <View style={styles.videoContent}>
+            <Text>No video</Text>
+          </View>
+        )}
+        {peer.isVadActive && (
+          <View style={styles.vadIcon}>
+            <Text>speaking</Text>
+          </View>
+        )}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginBottom: 10,
+  },
+  videoContent: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  video: {
+    flex: 1,
+    aspectRatio: 1,
+    borderRadius: 8,
+    overflow: 'hidden',
+    borderColor: '#001A72',
+    borderWidth: 2,
+  },
+  vadIcon: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    opacity: 0.5,
+  },
+  userLabel: {
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+    opacity: 0.5,
+    backgroundColor: '#F5F7FE',
+    borderRadius: 4,
+    padding: 3,
+  },
+});
