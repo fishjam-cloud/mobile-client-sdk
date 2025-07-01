9e4e7b4624db9bcba7142d591ebb347aad77d943
Dawid Matyjasik <63721806+dawidmatyjasik@users.noreply.github.com>
2025-07-01 12:27:17 +0200

    Feature/restructure example (#427)
    ## Description
    
    We moved the previous example to the `internal` folder and replaced it
    with a minimal, working example of a video room.
    
    ## Motivation and Context
    
    The previous version was overly complex, including many elements that
    were unnecessary for a public showcase
    
    ## How has this been tested?
    
    IOS and Android real devices + simulators
    
    ## Types of changes
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to
          not work as expected)
    
    ## Checklist:
    
    - [x] My code follows the code style of this project.
    - [x] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    
    ## Screenshots (if appropriate)
    <img width="452" alt="image"
    src="https://github.com/user-attachments/assets/cd733a2e-ee83-4421-9bbd-230b295137e7"
    />
    
    <img width="1428" alt="image"
    src="https://github.com/user-attachments/assets/700a2fc6-569f-4eeb-b6a0-b41ed6e3c324"
    />

---
+++ examples/video-room/components/VideosGridItem.tsx
@@ -0,0 +1,46 @@
+import { VideoRendererView } from '@fishjam-cloud/react-native-client';
+import { View, StyleSheet, Text } from 'react-native';
+import { GridTrack } from '../types';
+
+export const VideosGridItem = ({ peer }: { peer: GridTrack }) => {
+  return (
+    <View style={styles.container}>
+      <View
+        style={[
+          styles.video,
+          { backgroundColor: peer.isLocal ? '#606619' : '#7089DB' },
+        ]}>
+        {peer.track ? (
+          <VideoRendererView
+            trackId={peer.track.id}
+            videoLayout="FIT"
+            style={styles.videoContent}
+          />
+        ) : (
+          <View style={styles.videoContent}>
+            <Text>No video</Text>
+          </View>
+        )}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 0.5,
+  },
+  video: {
+    flex: 1,
+    aspectRatio: 1,
+    borderRadius: 8,
+    overflow: 'hidden',
+    borderColor: '#001A72',
+    borderWidth: 1,
+  },
+  videoContent: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
