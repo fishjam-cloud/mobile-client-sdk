d9a044f5cd898d8862638339798882b3666ebbaa
Anna Olak <anna.olak@swmansion.com>
2025-11-04 14:32:59 +0100

    FCE-2193: Display peer name in PIP on Android (#510)
    ## Description
    
    We want to display a peer name on the video in PIP on Android the same
    as it's done on iOS
    
    ## Documentation impact
    
    - [ ] Documentation update required
    - [ ] Documentation updated [in another PR](_)
    - [x] No documentation update required
    
    ## Types of changes
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to
          not work as expected)

--- packages/react-native-client/android/src/main/java/io/fishjam/reactnative/PipViewFactory.kt
+++ packages/react-native-client/android/src/main/java/io/fishjam/reactnative/PipViewFactory.kt
@@ -2,6 +2,8 @@ package io.fishjam.reactnative
 
 import android.content.Context
 import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
+import android.text.TextUtils
 import android.util.TypedValue
 import android.view.Gravity
 import android.view.View
@@ -19,7 +21,8 @@ data class PipViewContainer(
     val primaryVideoView: VideoRendererView,
     val secondaryVideoView: VideoRendererView,
     val primaryPlaceholder: TextView,
-    val secondaryPlaceholder: TextView
+    val secondaryPlaceholder: TextView,
+    val secondaryNameOverlay: TextView
 )
 
 class PipViewFactory(
@@ -31,6 +34,12 @@ class PipViewFactory(
         private const val PLACEHOLDER_TEXT_COLOR = Color.WHITE
         private const val PLACEHOLDER_TEXT_SIZE_SP = 16f
         private const val VIDEO_LAYOUT_MODE = "FILL"
+        
+        private const val NAME_OVERLAY_TEXT_SIZE_SP = 12f
+        private const val NAME_OVERLAY_PADDING_DP = 5
+        private const val NAME_OVERLAY_MAX_WIDTH_DP = 80
+        private const val NAME_OVERLAY_BG_COLOR = 0x4D000000
+        private const val NAME_OVERLAY_CORNER_RADIUS_DP = 5f
     }
 
     fun createPipViews(
@@ -45,6 +54,8 @@ class PipViewFactory(
 
         val primaryVideoView = createVideoView()
         val secondaryVideoView = createVideoView()
+        
+        val secondaryNameOverlay = createNameOverlayTextView()
 
         primaryContainer.apply {
             addView(primaryPlaceholder, createMatchParentLayoutParams())
@@ -54,6 +65,7 @@ class PipViewFactory(
         secondaryContainer.apply {
             addView(secondaryPlaceholder, createMatchParentLayoutParams())
             addView(secondaryVideoView, createCenteredLayoutParams())
+            addView(secondaryNameOverlay, createNameOverlayLayoutParams())
         }
 
         val splitScreenContainer = createSplitScreenContainer().apply {
@@ -68,7 +80,8 @@ class PipViewFactory(
             primaryVideoView = primaryVideoView,
             secondaryVideoView = secondaryVideoView,
             primaryPlaceholder = primaryPlaceholder,
-            secondaryPlaceholder = secondaryPlaceholder
+            secondaryPlaceholder = secondaryPlaceholder,
+            secondaryNameOverlay = secondaryNameOverlay
         )
     }
 
@@ -123,6 +136,51 @@ class PipViewFactory(
         ).apply {
             addRule(RelativeLayout.CENTER_IN_PARENT)
         }
+    }
+    
+    private fun createNameOverlayTextView(): TextView {
+        val paddingPx = dpToPx(NAME_OVERLAY_PADDING_DP)
+        val maxWidthPx = dpToPx(NAME_OVERLAY_MAX_WIDTH_DP)
+        val cornerRadiusPx = dpToPx(NAME_OVERLAY_CORNER_RADIUS_DP.toInt()).toFloat()
+        
+        val background = GradientDrawable().apply {
+            setColor(NAME_OVERLAY_BG_COLOR.toInt())
+            cornerRadii = floatArrayOf(
+                cornerRadiusPx, cornerRadiusPx,
+                0f, 0f,
+                0f, 0f,
+                0f, 0f
+            )
+        }
+        
+        return TextView(context).apply {
+            setTextColor(PLACEHOLDER_TEXT_COLOR)
+            setTextSize(TypedValue.COMPLEX_UNIT_SP, NAME_OVERLAY_TEXT_SIZE_SP)
+            setPadding(paddingPx, paddingPx, paddingPx, paddingPx)
+            maxWidth = maxWidthPx
+            maxLines = 1
+            ellipsize = TextUtils.TruncateAt.END
+            setBackground(background)
+            visibility = View.GONE
+        }
+    }
+    
+    private fun createNameOverlayLayoutParams(): RelativeLayout.LayoutParams {
+        return RelativeLayout.LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        ).apply {
+            addRule(RelativeLayout.ALIGN_PARENT_BOTTOM)
+            addRule(RelativeLayout.ALIGN_PARENT_END)
+        }
+    }
+    
+    private fun dpToPx(dp: Int): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp.toFloat(),
+            context.resources.displayMetrics
+        ).toInt()
     }
 }
 
