ade8f3f46a3a37f0fd77c193a88dae8d525a8421
Anna Olak <15946812+anna1901@users.noreply.github.com>
2025-12-02 12:34:51 +0100

    Implement proper livestream example app

---
+++ examples/video-player/components/FishjamPlayerStreamer.tsx
@@ -0,0 +1,68 @@
+import React, { useCallback, useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
+import {
+  LivestreamStreamer,
+  useLivestreamStreamer,
+  cameras,
+  VideoParameters,
+} from '@fishjam-cloud/react-native-client/livestream';
+import { useSandbox } from '@fishjam-cloud/react-native-client';
+
+interface FishjamPlayerStreamerProps {
+  roomName: string;
+}
+
+const FishjamPlayerStreamer = ({
+  roomName,
+}: FishjamPlayerStreamerProps) => {
+  const { getSandboxLivestream } = useSandbox({
+    fishjamId: process.env.EXPO_PUBLIC_FISHJAM_ID,
+  });
+
+  const {
+    connect,
+    disconnect,
+    whipClientRef,
+  } = useLivestreamStreamer({
+    videoEnabled: true,
+    audioEnabled: true,
+    camera: cameras[0],
+    videoParameters: VideoParameters.presetHD169,
+  });
+
+  const handleConnect = useCallback(async () => {
+    try {
+      const { streamerToken } = await getSandboxLivestream(roomName, false);
+      await connect(streamerToken);
+    } catch (err) {
+      console.log(err);
+    }
+  }, [connect, getSandboxLivestream, roomName]);
+
+  useEffect(() => {
+    handleConnect();
+
+    return () => {
+      disconnect();
+    };
+  }, [handleConnect, disconnect]);
+
+  return (
+    <View style={styles.playerContentContainer}>
+        <LivestreamStreamer
+          style={{ height: '90%', aspectRatio: 16 / 9 }}
+          ref={whipClientRef}
+      />
+    </View>
+  );
+};
+
+export default FishjamPlayerStreamer;
+
+const styles = StyleSheet.create({
+    playerContentContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  });
