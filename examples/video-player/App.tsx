af6bfb9bab718d1c70f9138d0fcb2b29045c8266
Anna Olak <anna.olak@swmansion.com>
2025-12-03 12:07:38 +0100

    Implement proper livestream example app (#525)
    ## Description
    
    Improve video-player example app so that it actually allows to stream
    and view streams.
    
    ## Motivation and Context
    
    The video-player example was not working. Now it shows how to use
    livestreaming functionality.
    
    ## Documentation impact
    
    - [ ] Documentation update required
    - [ ] Documentation updated [in another PR](_)
    - [x] No documentation update required
    
    ## Types of changes
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to
          not work as expected)

--- examples/video-player/App.tsx
+++ examples/video-player/App.tsx
@@ -1,12 +1,40 @@
-import { StyleSheet, View } from 'react-native';
-import FishjamPlayer from './components/FishjamPlayer';
-
-const roomName = 'test';
+import { StyleSheet, TextInput, View, Button } from 'react-native';
+import FishjamPlayerViewer from './components/FishjamPlayerViewer';
+import FishjamPlayerStreamer from './components/FishjamPlayerStreamer';
+import { useState } from 'react';
 
 const App = () => {
+  const [roomName, setRoomName] = useState('test-room');
+  const [selection, setSelection] = useState<'streamer' | 'viewer' | 'none'>(
+    'none',
+  );
+
   return (
-    <View style={styles.videoContainer}>
-      <FishjamPlayer roomName={roomName} />
+    <View style={styles.container}>
+      {selection === 'none' && (
+        <>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Room Name"
+            value={roomName}
+            onChangeText={setRoomName}
+          />
+          <Button title="Stream" onPress={() => setSelection('streamer')} />
+          <Button title="View stream" onPress={() => setSelection('viewer')} />
+        </>
+      )}
+      {selection === 'streamer' && (
+        <>
+          <FishjamPlayerStreamer roomName={roomName} />
+          <Button title="Back" onPress={() => setSelection('none')} />
+        </>
+      )}
+      {selection === 'viewer' && (
+        <>
+          <FishjamPlayerViewer roomName={roomName} />
+          <Button title="Back" onPress={() => setSelection('none')} />
+        </>
+      )}
     </View>
   );
 };
@@ -14,9 +42,22 @@ const App = () => {
 export default App;
 
 const styles = StyleSheet.create({
-  videoContainer: {
+  container: {
     flex: 1,
-    position: 'relative',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
     backgroundColor: 'black',
+  },
+  textInput: {
+    width: '100%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 8,
+    padding: 8,
+    color: 'white',
+    backgroundColor: 'black',
+    marginBottom: 16,
   },
 });
